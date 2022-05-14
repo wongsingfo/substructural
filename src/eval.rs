0030992b4285118d2708883473dcdec2757f210f
Light-of-Hers <724610746@qq.com>
2022-05-14 23:10:21 +0800

    implement eval

--- src/eval.rs
+++ src/eval.rs
@@ -1,47 +1,38 @@
 use crate::error::Error;
 use crate::syntax::{Qualifier, Term, TermCtx};
 use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct Binding {
-    name: String,
-    value: TermCtx,
-}
+use std::collections::HashMap;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Store {
-    bindings: Vec<Binding>,
+    bindings: HashMap<String, TermCtx>,
+    counter: u128,
 }
 
 impl Store {
     pub fn new_empty() -> Store {
         Store {
-            bindings: Vec::new(),
+            bindings: HashMap::new(),
+            counter: 0,
         }
     }
 
     fn push(&mut self, name: String, value: TermCtx) {
         assert!(get_qualifier(&value).is_some());
-        self.bindings.push(Binding { name, value });
+        self.bindings.insert(name, value);
     }
 
-    fn lookup(&mut self, name: &str) -> Option<TermCtx> {
-        let index = self
-            .bindings
-            .iter()
-            .rev()
-            .position(|binding| binding.name == name);
-        match index {
-            Some(index) => {
-                let binding = self.bindings.iter().rev().nth(index).unwrap();
-                if let Some(Qualifier::Linear) = get_qualifier(&binding.value) {
-                    Some(self.bindings.remove(index).value)
-                } else {
-                    Some(binding.value.clone())
-                }
-            }
-            None => None,
-        }
+    fn extract(&mut self, name: &str) -> Option<TermCtx> {
+        let value = self.bindings.get(name)?;
+        return match get_qualifier(value) {
+            Some(Qualifier::Linear) => self.bindings.remove(name),
+            _ => self.bindings.get(name).map(|x| x.clone()),
+        };
+    }
+
+    fn fresh_variable(&mut self, prefix: &str) -> String {
+        self.counter += 1;
+        format!("{}{}", prefix, self.counter)
     }
 }
 
@@ -63,7 +54,6 @@ impl From<TermCtx> for TermEval {
 fn is_value(term: &TermCtx) -> bool {
     let TermCtx(_, term) = term;
     match term {
-        Term::Variable(_) => true,
         Term::Boolean(_, _) => true,
         Term::Integer(_, _) => true,
         Term::Abstraction(_, _, _, _) => true,
@@ -73,118 +63,97 @@ fn is_value(term: &TermCtx) -> bool {
 
 fn get_qualifier(term: &TermCtx) -> Option<Qualifier> {
     let TermCtx(_, term) = term;
-    let qualifier = match term {
-        Term::Boolean(q, _) => Some(q),
-        Term::Integer(q, _) => Some(q),
-        Term::Abstraction(q, _, _, _) => Some(q),
-        _ => None,
+    let q = match term {
+        Term::Boolean(q, _) => q,
+        Term::Integer(q, _) => q,
+        Term::Abstraction(q, _, _, _) => q,
+        _ => return None,
     };
-    qualifier.map(|q| q.clone())
+    Some(*q)
 }
 
-fn eval_value(store: &mut Store, term_ctx: &TermCtx) -> Result<TermCtx, Error> {
+fn subst_var(term_ctx: Box<TermCtx>, x: &str, x2: &str) -> Box<TermCtx> {
+    let TermCtx(ctx, term) = *term_ctx;
+    let term = match term {
+        Term::Variable(y) if y == x => Term::Variable(x2.to_owned()),
+        Term::Application(t1, t2) => Term::Application(subst_var(t1, x, x2), subst_var(t2, x, x2)),
+        Term::Abstraction(q, y, ty, t) => Term::Abstraction(
+            q,
+            y.clone(),
+            ty,
+            if y == x { t } else { subst_var(t, x, x2) },
+        ),
+        Term::Conditional(t1, t2, t3) => Term::Conditional(
+            subst_var(t1, x, x2),
+            subst_var(t2, x, x2),
+            subst_var(t3, x, x2),
+        ),
+        _ => term,
+    };
+    Box::new(TermCtx(ctx, term))
+}
+
+fn one_step_eval_aux(store: &mut Store, term_ctx: TermCtx) -> Result<TermCtx, Error> {
+    let err = |msg| Error::EvaluateError {
+        message: msg,
+        source: term_ctx.0.to_string(),
+    };
+    let mut extract = |x: &str| -> Result<TermCtx, Error> {
+        store
+            .extract(x)
+            .ok_or_else(|| err(format!("Variable {} not found", x)))
+    };
     let TermCtx(ctx, term) = term_ctx;
-    match term {
-        Term::Variable(name) => match store.lookup(name) {
-            Some(value) => Ok(value),
-            None => Err(Error::EvaluateError {
-                message: format!("Variable {} not found", name),
-                source: ctx.to_string(),
-            }),
+    let term = match term {
+        Term::Variable(x) => extract(&x)?.1,
+        Term::Boolean(_, _) | Term::Integer(_, _) | Term::Abstraction(_, _, _, _) => {
+            let var = store.fresh_variable("%x");
+            store.push(var.clone(), TermCtx(ctx, term));
+            Term::Variable(var)
+            // term
+        }
+        Term::Conditional(t1, t2, t3) => match *t1 {
+            TermCtx(_, Term::Variable(x)) => match extract(&x)? {
+                TermCtx(_, Term::Boolean(_, v)) => return Ok(if v { *t2 } else { *t3 }),
+                _ => return Err(err(format!("Conditional term must be boolean"))),
+            },
+            _ => {
+                let t1 = one_step_eval_aux(store, *t1)?;
+                Term::Conditional(Box::new(t1), t2, t3)
+            }
         },
-        Term::Boolean(_, _) => Ok(term_ctx.to_owned()),
-        Term::Integer(_, _) => Ok(term_ctx.to_owned()),
-        Term::Abstraction(_, _, _, _) => Ok(term_ctx.to_owned()),
-        _ => Err(Error::EvaluateError {
-            message: format!("Value expected, but found {:?}", term_ctx),
-            source: ctx.to_string(),
-        }),
-    }
+        Term::Application(t1, t2) => match (&*t1, &*t2) {
+            (TermCtx(_, Term::Variable(x1)), TermCtx(_, Term::Variable(x2))) => match extract(&x1)?
+            {
+                TermCtx(_, Term::Abstraction(_, x, _, body)) => {
+                    return Ok(*subst_var(body, &x, &x2))
+                }
+                _ => return Err(err(format!("Expect abstraction"))),
+            },
+            (TermCtx(_, Term::Variable(_)), _) => {
+                Term::Application(t1, Box::new(one_step_eval_aux(store, *t2)?))
+            }
+            _ => Term::Application(Box::new(one_step_eval_aux(store, *t1)?), t2),
+        },
+        _ => return Err(err(format!("Unknown term"))),
+    };
+    Ok(TermCtx(ctx, term))
 }
 
 pub(crate) fn one_step_eval(term_eval: TermEval) -> Result<TermEval, Error> {
-    let TermEval {
-        mut store,
-        term: TermCtx(ctx, term),
-    } = term_eval;
-    let term_ = match term {
-        Term::Variable(_) => {
-            let TermCtx(_, term) = eval_value(&mut store, &TermCtx(ctx.clone(), term))?;
-            term
-        }
-        Term::Boolean(_, _) => term,
-        Term::Integer(_, _) => term,
-        Term::Abstraction(_, _, _, _) => term,
-        Term::Conditional(t1, t2, t3) => {
-            if !is_value(&*t1) {
-                let TermEval { term: t1_, .. } = one_step_eval(TermEval {
-                    store: store.clone(),
-                    term: *t1,
-                })?;
-                Term::Conditional(Box::new(t1_), t2, t3)
-            } else {
-                let t1_ = eval_value(&mut store, &t1)?;
-                let TermCtx(ctx, t1_) = t1_;
-                let t1_ = match t1_ {
-                    Term::Boolean(_qualifier, v) => v,
-                    _ => {
-                        return Err(Error::EvaluateError {
-                            message: "Conditional term must be boolean".to_owned(),
-                            source: ctx.to_string(),
-                        })
-                    }
-                };
-                let TermCtx(_, t2_) = *t2;
-                let TermCtx(_, t3_) = *t3;
-                if t1_ {
-                    t2_
-                } else {
-                    t3_
-                }
-            }
-        }
-        Term::Application(t1, t2) => {
-            if !is_value(&*t1) {
-                let TermEval { term: t1_, .. } = one_step_eval(TermEval {
-                    store: store.clone(),
-                    term: *t1,
-                })?;
-                Term::Application(Box::new(t1_), t2)
-            } else if !is_value(&*t2) {
-                let TermEval { term: t2_, .. } = one_step_eval(TermEval {
-                    store: store.clone(),
-                    term: *t2,
-                })?;
-                Term::Application(t1, Box::new(t2_))
-            } else {
-                let t1_ = eval_value(&mut store, &t1)?;
-                let t2_ = eval_value(&mut store, &t2)?;
-                let TermCtx(_, t1_) = t1_;
-                let TermCtx(_, t2_) = t2_;
-                if let Term::Abstraction(_qualifier, v, _type, body) = t1_ {
-                    store.push(v, TermCtx(ctx.clone(), t2_));
-                    let TermCtx(_, body) = *body;
-                    body
-                } else {
-                    return Err(Error::EvaluateError {
-                        message: "Expected abstraction".to_owned(),
-                        source: ctx.to_string(),
-                    });
-                }
-            }
-        }
+    let TermEval { mut store, term } = term_eval;
+    let term = if is_value(&term) {
+        term
+    } else {
+        one_step_eval_aux(&mut store, term)?
     };
-    let result = TermEval {
-        store,
-        term: TermCtx(ctx, term_),
-    };
-    Ok(result)
+    Ok(TermEval { store, term })
 }
 
 #[cfg(test)]
 mod test {
-    use crate::syntax::parse_program;
     use crate::formatter::TermFormatter;
+    use crate::syntax::parse_program;
 
     use super::*;
 
@@ -214,14 +183,12 @@ mod test {
         let result = TermEval { store, term };
         let result = one_step_eval(result).unwrap();
         let result = one_step_eval(result).unwrap();
+        let result = one_step_eval(result).unwrap();
+        let result = one_step_eval(result).unwrap();
         println!("{:#?}", result);
-        assert!(
-            result.store.bindings.is_empty(),
-            "Store should be empty because `bool` is linear"
-        );
     }
 
-    #[ignore] // TODO(crz): help
+    // #[ignore] // TODO(crz): help
     #[test]
     fn test_eval_closure() {
         let store = Store::new_empty();
@@ -230,10 +197,58 @@ mod test {
         let term = parse_program(input).unwrap();
         let result = TermEval { store, term };
         let result = one_step_eval(result).unwrap();
-        println!("{:?} | {}", result.store,formatter.format_termctx(&result.term));
+        println!(
+            "{:?} | {}",
+            result.store,
+            formatter.format_termctx(&result.term)
+        );
         let result = one_step_eval(result).unwrap();
-        println!("{:?} | {}", result.store,formatter.format_termctx(&result.term));
+        println!(
+            "{:?} | {}",
+            result.store,
+            formatter.format_termctx(&result.term)
+        );
         let result = one_step_eval(result).unwrap();
-        println!("{:?} | {}", result.store,formatter.format_termctx(&result.term));
+        println!(
+            "{:?} | {}",
+            result.store,
+            formatter.format_termctx(&result.term)
+        );
+        let result = one_step_eval(result).unwrap();
+        println!(
+            "{:?} | {}",
+            result.store,
+            formatter.format_termctx(&result.term)
+        );
+        let result = one_step_eval(result).unwrap();
+        println!(
+            "{:?} | {}",
+            result.store,
+            formatter.format_termctx(&result.term)
+        );
+        let result = one_step_eval(result).unwrap();
+        println!(
+            "{:?} | {}",
+            result.store,
+            formatter.format_termctx(&result.term)
+        );
+        let result = one_step_eval(result).unwrap();
+        println!(
+            "{:?} | {}",
+            result.store,
+            formatter.format_termctx(&result.term)
+        );
+        let result = one_step_eval(result).unwrap();
+        println!(
+            "{:?} | {}",
+            result.store,
+            formatter.format_termctx(&result.term)
+        );
+        let result = one_step_eval(result).unwrap();
+        println!(
+            "{:?} | {}",
+            result.store,
+            formatter.format_termctx(&result.term)
+        );
     }
 }
